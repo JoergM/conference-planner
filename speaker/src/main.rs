6b95708989541ea0f0d6e2c7e06892581e8c4725
Joerg Mueller <joergm@hey.com>
2021-02-20 09:00:50 +0100

    Speaker service is giving the first answer.

--- speaker/src/main.rs
+++ speaker/src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+use actix_service::Service;
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
+use rand::Rng;
+use serde::Serialize;
+use std::env;
+use std::{thread, time};
+
+#[derive(Debug, Clone)]
+struct AppState {
+    speakers: Vec<Speaker>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+struct Speaker {}
+
+#[get("/")]
+async fn hello(scope: web::Data<AppState>) -> impl Responder {
+    let json = serde_json::to_string(&scope.speakers).unwrap();
+
+    HttpResponse::Ok().body(json)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    //reading Content from environment
+    let failure_rate_env = env::var("FAILURE_RATE").unwrap_or("0".to_string());
+    let failure_rate: i32 = failure_rate_env.parse().unwrap();
+    let random_delay_env = env::var("RANDOM_DELAY_MAX").unwrap_or("1".to_string());
+    let random_delay_max: u64 = random_delay_env.parse().unwrap();
+
+    //load Speakers Array
+
+    let speakers: Vec<Speaker> = Vec::new();
+
+    //initialize App_State
+    let app_state = AppState { speakers };
+
+    //Initialize Logger
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap_fn(move |req, srv| {
+                let fut = srv.call(req);
+                let mut rng = rand::thread_rng();
+                let failure = rng.gen_range(0..100) < failure_rate;
+                async move {
+                    let mut service_res = fut.await?;
+
+                    if failure {
+                        *service_res.response_mut() = HttpResponse::ServiceUnavailable().finish();
+                    }
+                    Ok(service_res)
+                }
+            })
+            .wrap_fn(move |req, srv| {
+                let fut = srv.call(req);
+                let mut rng = rand::thread_rng();
+                let delay = time::Duration::from_millis(rng.gen_range(0..random_delay_max));
+                async move {
+                    let service_res = fut.await?;
+                    thread::sleep(delay);
+                    Ok(service_res)
+                }
+            })
+            .wrap(Logger::default())
+            .data(app_state.clone())
+            .service(hello)
+    })
+    .bind("127.0.0.1:8081")?
+    .run()
+    .await
 }
