c00b35122c4895a8305d95f6d51c7802fd6b9f2a
Joerg Mueller <joergm@hey.com>
2021-02-26 11:32:30 +0100

    Build and push now includes version tagging.

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,27 +1,32 @@
-#! /bin/sh
+#! /bin/sh 
 
 function error() {
     echo $1
     exit
 }
 
+function build_and_push_module() {
+DOCKER_PREFIX=$1
+MODULE_NAME=$2
+
+cd ${MODULE_NAME}
+MODULE_VERSION=$(cargo pkgid | cut -d# -f2 | cut -d: -f2)
+
+docker build -t ${DOCKER_PREFIX}/${MODULE_NAME}:${MODULE_VERSION} .
+docker tag ${DOCKER_PREFIX}/${MODULE_NAME}:${MODULE_VERSION} ${DOCKER_PREFIX}/${MODULE_NAME}:latest
+docker push ${DOCKER_PREFIX}/${MODULE_NAME} 
+
+cd ..
+}
+
+#main
+
 DOCKER_PREFIX=$1 
 [ -z "$DOCKER_PREFIX" ] && error "Please enter Docker Prefix as first argument (See README.md)"
 
-cd frontend
-docker build -t ${DOCKER_PREFIX}/frontend .
-docker push ${DOCKER_PREFIX}/frontend 
-
-cd ../schedule
-docker build -t ${DOCKER_PREFIX}/schedule .
-docker push ${DOCKER_PREFIX}/schedule 
-
-cd ../speaker
-docker build -t ${DOCKER_PREFIX}/speaker .
-docker push ${DOCKER_PREFIX}/speaker 
-
-cd ../session
-docker build -t ${DOCKER_PREFIX}/session .
-docker push ${DOCKER_PREFIX}/session 
+build_and_push_module ${DOCKER_PREFIX} frontend
+build_and_push_module ${DOCKER_PREFIX} schedule
+build_and_push_module ${DOCKER_PREFIX} speaker
+build_and_push_module ${DOCKER_PREFIX} session
 
 
