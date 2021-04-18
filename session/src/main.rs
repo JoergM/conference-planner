11537a66844f270b16c7e4170cd7378c1eab00b5
Joerg Mueller <joergm@hey.com>
2021-04-18 15:38:48 +0200

    Session uses awc as client and sets Open Telemetry spans.

--- session/src/main.rs
+++ session/src/main.rs
@@ -10,9 +10,25 @@ use std::{thread, time};
 mod session;
 use session::*;
 
+use actix_web_opentelemetry::{ClientExt, RequestTracing};
+use opentelemetry::Context;
+
 #[derive(Debug, Clone)]
 struct AppState {
     sessions: Vec<Session>,
+}
+
+async fn get_body_with_tracing(url: &str) -> String {
+    let client = awc::Client::default();
+    let mut resp = client
+        .get(url)
+        .trace_request_with_context(Context::current())
+        .send()
+        .await
+        .unwrap();
+    let body = resp.body().await.unwrap();
+    let body_text = String::from_utf8(body.to_vec()).unwrap();
+    body_text
 }
 
 #[derive(Serialize)]
@@ -26,19 +42,16 @@ struct SessionAnswer {
     speaker_twitter: String,
 }
 
-impl From<Session> for SessionAnswer {
-    fn from(session: Session) -> Self {
-        //todo implement request to speakers
-        let data = get_speaker_value(session.speaker_id);
-
-        let speaker_name = data["full_name"].as_str().unwrap_or("");
-        let speaker_twitter = data["twitter"].as_str().unwrap_or("");
+impl SessionAnswer {
+    fn new(session: &Session, speaker_data: Map<String, Value>) -> Self {
+        let speaker_name = speaker_data["full_name"].as_str().unwrap_or("");
+        let speaker_twitter = speaker_data["twitter"].as_str().unwrap_or("");
 
         SessionAnswer {
             id: session.id,
-            title: session.title,
-            tag: session.tag,
-            description: session.description,
+            title: session.title.clone(),
+            tag: session.tag.clone(),
+            description: session.description.clone(),
             speaker_id: session.speaker_id,
             speaker_name: speaker_name.into(),
             speaker_twitter: speaker_twitter.into(),
@@ -46,21 +59,23 @@ impl From<Session> for SessionAnswer {
     }
 }
 
-fn get_speaker_value(id: u32) -> Map<String, Value> {
+async fn get_speaker_value(id: u32) -> Map<String, Value> {
     let url = format!("http://speakers:8081/{}", id);
-    let resp = reqwest::blocking::get(&url).unwrap();
-    let data: Map<String, Value> = serde_json::from_str(&resp.text().unwrap()).unwrap();
+    let body_text = get_body_with_tracing(&url).await;
+    let data: Map<String, Value> = serde_json::from_str(&body_text).unwrap();
 
     data
 }
 
 #[get("/")]
 async fn list(scope: web::Data<AppState>) -> impl Responder {
-    let answers: Vec<SessionAnswer> = scope
-        .sessions
-        .iter()
-        .map(|session| SessionAnswer::from(session.clone()))
-        .collect();
+    let mut answers: Vec<SessionAnswer> = Vec::new();
+
+    for session in scope.sessions.iter() {
+        let speaker_data = get_speaker_value(session.id).await;
+        let session_answer = SessionAnswer::new(session, speaker_data);
+        answers.push(session_answer)
+    }
     let json = serde_json::to_string(&answers).unwrap();
 
     HttpResponse::Ok().body(json)
@@ -71,10 +86,12 @@ async fn session_by_id(
     web::Path(id): web::Path<u32>,
     scope: web::Data<AppState>,
 ) -> impl Responder {
-    let session = scope.sessions.iter().find(|session| session.id == id);
+    let session_opt = scope.sessions.iter().find(|session| session.id == id);
 
-    if session.is_some() {
-        let session_answer = SessionAnswer::from(session.unwrap().clone());
+    if session_opt.is_some() {
+        let session = session_opt.unwrap();
+        let speaker_data = get_speaker_value(session.id).await;
+        let session_answer = SessionAnswer::new(session, speaker_data);
         let json = serde_json::to_string(&session_answer).unwrap();
         HttpResponse::Ok().body(json)
     } else {
@@ -94,6 +111,15 @@ async fn main() -> std::io::Result<()> {
     let app_state = AppState {
         sessions: session::generate_examples(),
     };
+
+    // register opentelemetry collector
+    let collector_env =
+        env::var("OTEL_EXPORTER_JAEGER_ENDPOINT").unwrap_or("localhost:14268".to_string());
+    let (_tracer, _uninstall) = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("Session")
+        .with_collector_endpoint(format!("http://{}/api/traces", collector_env))
+        .install()
+        .unwrap();
 
     //Initialize Logger
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -123,6 +149,7 @@ async fn main() -> std::io::Result<()> {
                     Ok(service_res)
                 }
             })
+            .wrap(RequestTracing::new())
             .wrap(Logger::default())
             .data(app_state.clone())
             .service(list)
