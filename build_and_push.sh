9060d9b48fd1c1f5649a78597c105e5f42a4bb6a
Joerg Mueller <joergm@hey.com>
2022-03-30 07:23:08 +0200

    Fixed docker build.

--- build_and_push.sh
+++ build_and_push.sh
@@ -11,13 +11,13 @@ MODULE_NAME=$2
 
 cd ${MODULE_NAME}
 MODULE_VERSION=$(cargo pkgid | cut -d# -f2 | cut -d: -f2)
+cd ..
 
-docker build -t ${DOCKER_PREFIX}/${MODULE_NAME}:${MODULE_VERSION} . || error "Error building Image"
+docker build . -t ${DOCKER_PREFIX}/${MODULE_NAME}:${MODULE_VERSION} -f Dockerfile-${MODULE_NAME} || error "Error building Image"
 docker tag ${DOCKER_PREFIX}/${MODULE_NAME}:${MODULE_VERSION} ${DOCKER_PREFIX}/${MODULE_NAME}:latest
 docker push ${DOCKER_PREFIX}/${MODULE_NAME}:${MODULE_VERSION} || error "Error pushing new version"
 docker push ${DOCKER_PREFIX}/${MODULE_NAME} || error "Error pushing latest"
 
-cd ..
 }
 
 #main
@@ -25,8 +25,8 @@ cd ..
 DOCKER_PREFIX=$1 
 [ -z "$DOCKER_PREFIX" ] && error "Please enter Docker Prefix as first argument (See README.md)"
 
-build_and_push_module ${DOCKER_PREFIX} frontend
 build_and_push_module ${DOCKER_PREFIX} schedule
+build_and_push_module ${DOCKER_PREFIX} frontend
 build_and_push_module ${DOCKER_PREFIX} speaker
 build_and_push_module ${DOCKER_PREFIX} session
 build_and_push_module ${DOCKER_PREFIX} loadgenerator
